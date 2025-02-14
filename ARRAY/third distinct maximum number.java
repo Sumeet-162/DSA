d0a5d1d16d9d9433c7d4bff1fa5cf241d198e93d
Sumeet <117519961+Sumeet-162@users.noreply.github.com>
2025-02-15 00:13:17 +0530

    Create  third distinct maximum number.java

---
+++ ARRAY/third distinct maximum number.java
@@ -0,0 +1,54 @@
+// 414. Third Maximum Number
+// Solved
+// Easy
+// Topics
+// Companies
+// Given an integer array nums, return the third distinct maximum number in this array. If the third maximum does not exist, return the maximum number.
+
+ 
+
+// Example 1:
+
+// Input: nums = [3,2,1]
+// Output: 1
+// Explanation:
+// The first distinct maximum is 3.
+// The second distinct maximum is 2.
+// The third distinct maximum is 1.
+// Example 2:
+
+// Input: nums = [1,2]
+// Output: 2
+// Explanation:
+// The first distinct maximum is 2.
+// The second distinct maximum is 1.
+// The third distinct maximum does not exist, so the maximum (2) is returned instead.
+// Example 3:
+
+// Input: nums = [2,2,3,1]
+// Output: 1
+// Explanation:
+// The first distinct maximum is 3.
+// The second distinct maximum is 2 (both 2's are counted together since they have the same value).
+// The third distinct maximum is 1.
+
+class Solution {
+    public int thirdMax(int[] nums) {
+        TreeSet<Integer> set = new TreeSet<>();
+
+        for(int num : nums){
+            set.add(num);
+
+            if(set.size()>3){
+                set.pollFirst();
+            }
+        }
+
+        if(set.size()==3){
+            return set.first();
+        }
+        else{
+            return set.last();
+        }
+    }
+}
